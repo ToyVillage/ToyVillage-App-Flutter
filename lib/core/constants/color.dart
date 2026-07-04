3ea929e5794de77afd59e58d319d2f5bd8777da8
leejia0324 <hx.un0707@gmail.com>
2026-07-05 03:02:27 +0900

    feat: 컬러 시스템 추가

---
+++ lib/core/constants/color.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+class ToyVillageColor {
+  static const red = Color(0xFFFF3131);
+  static const redBackground = Color(0xFFFFCECE);
+  static const yellow = Color(0xFFFDB542);
+  static const yellowBackground = Color(0xFFFFE8C3);
+  static const green = Color(0xFF00B48A);
+  static const greenBackground = Color(0xFFA9ECDD);
+  static const blue = Color(0xFF4952FF);
+
+  static const gray10 = Color(0xFFF5F5F7);
+  static const gray20 = Color(0xFFDDDDE2);
+  static const gray30 = Color(0xFFC6C6CE);
+  static const gray40 = Color(0xFFAFAFB9);
+  static const gray50 = Color(0xFF9999A5);
+  static const gray60 = Color(0xFF848491);
+  static const gray70 = Color(0xFF6F6F7C);
+  static const gray80 = Color(0xFF5B5B68);
+  static const gray90 = Color(0xFF484853);
+  static const gray100 = Color(0xFF36363F);
+}
