42e11b92d37c52d6a57110ff4c63db485ffb1c17
leejia0324 <hx.un0707@gmail.com>
2026-07-05 21:16:47 +0900

    feat: 첨부파일 컴포넌트 추가

---
+++ lib/features/notice/presentation/widget/file_attachment.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:toy_village_app/core/constants/color.dart';
+import 'package:toy_village_app/core/constants/text_style.dart';
+import 'package:toy_village_app/core/utils/file_util.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class FileAttachment extends StatelessWidget {
+  final String fileName;
+  final VoidCallback onDownload;
+
+  const FileAttachment({
+    super.key,
+    required this.fileName,
+    required this.onDownload,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final fileType = FileType.fromFileName(fileName);
+
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: ToyVillageColor.gray60)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Row(
+          children: [
+            SvgPicture.asset(fileType.icon),
+            const SizedBox(width: 8,),
+            Expanded(
+              child: Text(
+                fileName,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: ToyVillageTextStyle.button4,
+              ),
+            ),
+            const SizedBox(width: 8,),
+            GestureDetector(
+              onTap: onDownload,
+              child: const Icon(
+                Symbols.download,
+                size: 24,
+                color: ToyVillageColor.gray100,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
