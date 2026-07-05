fecdcc3072a7bc58d8dcad6a1c5d4b3c1ef2b455
leejia0324 <hx.un0707@gmail.com>
2026-07-05 17:09:30 +0900

    fix: font weight 재설정

--- lib/core/constants/text_style.dart
+++ lib/core/constants/text_style.dart
@@ -5,132 +5,132 @@ abstract final class ToyVillageTextStyle {
   /// Heading
   static TextStyle heading1 = defaultTextStyle.copyWith(
     fontSize: 32,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
 
   static TextStyle heading2 = defaultTextStyle.copyWith(
     fontSize: 28,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
 
   static TextStyle heading3 = defaultTextStyle.copyWith(
     fontSize: 24,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
 
   static TextStyle heading4 = defaultTextStyle.copyWith(
     fontSize: 22,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
 
   /// SubTitle
   static TextStyle subTitle1 = defaultTextStyle.copyWith(
     fontSize: 24,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
-  static TextStyle subTItle2 = defaultTextStyle.copyWith(
+  static TextStyle subTitle2 = defaultTextStyle.copyWith(
     fontSize: 22,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle subTitle3 = defaultTextStyle.copyWith(
     fontSize: 20,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle subTitle4 = defaultTextStyle.copyWith(
     fontSize: 18,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   /// Body
   static TextStyle body1 = defaultTextStyle.copyWith(
     fontSize: 24,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle body2 = defaultTextStyle.copyWith(
     fontSize: 22,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle body3 = defaultTextStyle.copyWith(
     fontSize: 20,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle body4 = defaultTextStyle.copyWith(
     fontSize: 18,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle body5 = defaultTextStyle.copyWith(
     fontSize: 16,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   /// Button
   static TextStyle button1 = defaultTextStyle.copyWith(
     fontSize: 22,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle button2 = defaultTextStyle.copyWith(
     fontSize: 20,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle button3 = defaultTextStyle.copyWith(
     fontSize: 18,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle button4 = defaultTextStyle.copyWith(
     fontSize: 16,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle button5 = defaultTextStyle.copyWith(
     fontSize: 14,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   /// Caption
   static TextStyle caption1 = defaultTextStyle.copyWith(
     fontSize: 20,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle caption2 = defaultTextStyle.copyWith(
     fontSize: 18,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle caption3 = defaultTextStyle.copyWith(
     fontSize: 16,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle caption4 = defaultTextStyle.copyWith(
     fontSize: 14,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
   /// Calendar
-  static TextStyle calendar1 = defaultTextStyle.copyWith(
+  static TextStyle calendarWeek = defaultTextStyle.copyWith(
     fontSize: 14,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
   );
 
-  static TextStyle calendar2 = defaultTextStyle.copyWith(
+  static TextStyle calendarEnable = defaultTextStyle.copyWith(
     fontSize: 16,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
-  
-  static TextStyle calendar3 = defaultTextStyle.copyWith(
+
+  static TextStyle calendarDisable = defaultTextStyle.copyWith(
     fontSize: 14,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.w600,
   );
 }
 
