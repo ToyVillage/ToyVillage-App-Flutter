a92ada4bed6f445e6d43b30452b3cd15fe9acdc3
leejia0324 <hx.un0707@gmail.com>
2026-07-07 01:05:35 +0900

    fix: state.extra 캐스팅 안전하게 수정

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:toy_village_app/features/notice/presentation/view/notice_detail_view.dart';
@@ -16,7 +17,13 @@ final GoRouter appRouter = GoRouter(
         GoRoute(
           path: 'detail',
           builder: (context, state) {
-            return NoticeDetailView(id: state.extra as int);
+            final id = state.extra;
+            if (id is! int) {
+              return const Scaffold(
+                body: Center(child: Text('잘못된 접근입니다.')),
+              );
+            }
+            return NoticeDetailView(id: id);
           },
         ),
       ],
