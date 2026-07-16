0128ccb3b838c0c29a60b08f0b5d862cb19f6f72
leejia0324 <hx.un0707@gmail.com>
2026-07-16 23:54:08 +0900

    feat: file util에 서버 타입으로 받는 로직 추가

--- lib/core/utils/file_util.dart
+++ lib/core/utils/file_util.dart
@@ -1,15 +1,21 @@
+import 'dart:ui';
+
+import 'package:toy_village_app/core/constants/color.dart';
+
 enum FileType {
-  pdf(label: 'PDF', icon: 'assets/svg/pdf.svg'),
-  jpg(label: 'JPG', icon: 'assets/svg/jpg.svg'),
-  png(label: 'PNG', icon: 'assets/svg/png.svg'),
-  etc(label: 'FILE', icon: 'assets/svg/etc.svg');
+  pdf(label: 'PDF', icon: 'assets/svg/pdf.svg', backgroundColor: ToyVillageColor.redBackground),
+  jpg(label: 'JPG', icon: 'assets/svg/jpg.svg', backgroundColor: ToyVillageColor.yellowBackground),
+  png(label: 'PNG', icon: 'assets/svg/png.svg', backgroundColor: ToyVillageColor.greenBackground),
+  etc(label: 'FILE', icon: 'assets/svg/etc.svg', backgroundColor: ToyVillageColor.gray20);
 
   final String label;
   final String icon;
+  final Color backgroundColor;
 
   const FileType({
     required this.label,
     required this.icon,
+    required this.backgroundColor,
   });
 
   factory FileType.fromFileName(String fileName) {
@@ -27,6 +33,16 @@ enum FileType {
         return FileType.png;
       default:
         return FileType.etc;
+    }
+  }
+
+  factory FileType.fromType(String type) {
+    switch (type.toUpperCase()) {
+      case 'PDF': return FileType.pdf;
+      case 'JPG':
+      case 'JPEG': return FileType.jpg;
+      case 'PNG': return FileType.png;
+      default: return FileType.etc;
     }
   }
 }
