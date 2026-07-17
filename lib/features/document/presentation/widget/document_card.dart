0951382fc97064f19073bca013d36b934194c923
leejia0324 <hx.un0707@gmail.com>
2026-07-17 23:31:05 +0900

    feat: document card 제작

---
+++ lib/features/document/presentation/widget/document_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:toy_village_app/core/constants/color.dart';
+import 'package:toy_village_app/core/constants/text_style.dart';
+import 'package:toy_village_app/core/utils/file_util.dart';
+
+class DocumentCard extends StatelessWidget {
+  final String title;
+  final String type;
+
+  const DocumentCard({
+    super.key,
+    required this.title,
+    required this.type
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final fileType = FileType.fromType(type);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(color: ToyVillageColor.white),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: fileType.backgroundColor,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset(fileType.icon),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 35),
+                  child: Text(title, style: ToyVillageTextStyle.heading5),
+                ),
+              ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {},
+                  borderRadius: BorderRadius.circular(4),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      border: Border.all(color: ToyVillageColor.gray60),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8,
+                        vertical: 6,
+                      ),
+                      child: Text(
+                        '미리보기',
+                        style: ToyVillageTextStyle.button5.copyWith(
+                          color: ToyVillageColor.gray60,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
