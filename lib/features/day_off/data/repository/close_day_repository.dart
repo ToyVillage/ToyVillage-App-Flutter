6f5f5ab0b6ecbc32d38aff912dc22185f063275b
leejia0324 <hx.un0707@gmail.com>
2026-07-19 00:56:41 +0900

    feat: 휴관일 API 연동

--- lib/features/day_off/data/repository/close_day_repository.dart
+++ lib/features/day_off/data/repository/close_day_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:toy_village_app/core/network/api_endpoints.dart';
 import 'package:toy_village_app/core/network/dio_provider.dart';
 import 'package:toy_village_app/features/day_off/data/model/close_day_model.dart';
 
@@ -7,33 +8,15 @@ final closeDayRepositoryProvider =
     Provider((ref) => CloseDayRepository(ref.read(dioProvider)));
 
 class CloseDayRepository {
-  // ignore: unused_field
   final Dio _dio;
 
   CloseDayRepository(this._dio);
 
   Future<List<CloseDayModel>> loadCloseDays() async {
-    // final res = await _dio.get(ApiEndpoints.closeDay);
-    // final list = res.data as List;
-    // return list
-    //     .map((e) => CloseDayModel.fromJson(e as Map<String, dynamic>))
-    //     .toList();
-
-    await Future.delayed(const Duration(milliseconds: 300));
-    final dummy = <Map<String, dynamic>>[
-      {
-        'id': 1,
-        'title': '정기 휴관',
-        'startCloseTime': '2026-07-09',
-        'endCloseTime': '2026-07-09',
-      },
-      {
-        'id': 2,
-        'title': '시설 점검',
-        'startCloseTime': '2026-07-15',
-        'endCloseTime': '2026-07-16',
-      },
-    ];
-    return dummy.map(CloseDayModel.fromJson).toList();
+    final res = await _dio.get(ApiEndpoints.closeDay);
+    final list = res.data as List;
+    return list
+        .map((e) => CloseDayModel.fromJson(e as Map<String, dynamic>))
+        .toList();
   }
 }
