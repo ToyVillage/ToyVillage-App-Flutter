c48a15e084e4e650c662c0b1a5fb066d47d068d4
leejia0324 <hx.un0707@gmail.com>
2026-07-21 16:04:30 +0900

    feat: icon svg 파일 추가

---
+++ lib/core/constants/svg_assets.dart
@@ -0,0 +1,14 @@
+class SvgAssets {
+  static const pdf = 'assets/svg/pdf.svg';
+  static const jpg = 'assets/svg/jpg.svg';
+  static const png = 'assets/svg/png.svg';
+  static const etc = 'assets/svg/etc.svg';
+  static const dateToday = 'assets/svg/date_today.svg';
+  static const people = 'assets/svg/people.svg';
+  static const book = 'assets/svg/book.svg';
+  static const flag = 'assets/svg/flag.svg';
+  static const location = 'assets/svg/location.svg';
+  static const out = 'assets/svg/out.svg';
+  static const clock = 'assets/svg/clock.svg';
+  static const clockCheck = 'assets/svg/clock_check.svg';
+}
