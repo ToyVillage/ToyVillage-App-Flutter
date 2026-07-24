9de598a2214d9de8c993b3a3d79e3fa88fcb304e
leejia0324 <hx.un0707@gmail.com>
2026-07-24 21:17:21 +0900

    fix: 자료실 파일 미리보기 및 다운로드 실제 동작 구현
    - fileKey를 CDN URL로 변환하여 실제 파일 로드
    - 미리보기: 이미지는 Image.network, PDF는 iOS 직접·Android Docs 뷰어 경유
    - 다운로드: 이미지는 사진 앱/갤러리, 그 외는 Android Downloads·iOS 저장 시트
    - gal, flutter_file_downloader 추가 및 사진/저장 권한 설정

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import gal
+import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
