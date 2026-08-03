5cbcb3ca816836723af9dada8b11909807fd0420
leejia0324 <hx.un0707@gmail.com>
2026-08-04 08:14:06 +0900

    feat: file 레포지토리 추가

---
+++ lib/core/network/file_repository.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:toy_village_app/core/network/api_endpoints.dart';
+import 'package:toy_village_app/core/network/dio_provider.dart';
+
+final fileRepositoryProvider =
+    Provider((ref) => FileRepository(ref.read(dioProvider)));
+
+class FileRepository {
+  final Dio _dio;
+
+  FileRepository(this._dio);
+
+  Future<String> upload(String path, String fileName) async {
+    final formData = FormData.fromMap({
+      'files': await MultipartFile.fromFile(path, filename: fileName),
+    });
+    final res = await _dio.post(ApiEndpoints.file, data: formData);
+    return res.data['fileKey'] as String;
+  }
+}
