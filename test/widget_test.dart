9cf24e6760da631aeea32a3f3ffb1b6f39975543
leejia0324 <hx.un0707@gmail.com>
2026-08-19 09:01:12 +0900

    test: 로그인 화면 렌더 스모크 테스트

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:toy_village_app/main.dart';
 
 void main() {
-  testWidgets('App renders', (WidgetTester tester) async {
-    await tester.pumpWidget(const ToyVillageApp());
+  testWidgets('앱이 로그인 화면으로 렌더된다', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(child: ToyVillageApp()));
+    await tester.pump();
 
-    expect(find.text('ToyVillage'), findsWidgets);
+    expect(find.text('로그인'), findsWidgets);
   });
 }
