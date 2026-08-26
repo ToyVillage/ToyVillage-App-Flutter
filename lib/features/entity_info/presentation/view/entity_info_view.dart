fd3290e6f4a142d62d1b27764ad9d0f243ea2459
leejia0324 <hx.un0707@gmail.com>
2026-08-26 15:27:47 +0900

    feat: 개체 정보 페이지 퍼블리싱

--- lib/features/entity_info/presentation/view/entity_info_view.dart
+++ lib/features/entity_info/presentation/view/entity_info_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:toy_village_app/core/widgets/app_bar/app_bar.dart';
 import 'package:toy_village_app/core/widgets/chip/category_filter.dart';
 import 'package:toy_village_app/core/widgets/text/title.dart';
+import 'package:toy_village_app/features/entity_info/presentation/widget/entity_card.dart';
 
 class EntityInfoView extends StatefulWidget {
   const EntityInfoView({super.key});
@@ -22,14 +24,26 @@ class _EntityInfoViewState extends State<EntityInfoView> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const ToyVillageTitle(
-              title: '개체 정보',
-              subTitle: '동물원 내 동물을 확인합니다',
-            ),
+            const ToyVillageTitle(title: '개체 정보', subTitle: '동물원 내 동물을 확인합니다'),
             const SizedBox(height: 28),
             CategoryFilter(
               selected: _category,
               onSelected: (value) => setState(() => _category = value),
+            ),
+            const SizedBox(height: 18),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 4,
+                itemBuilder: (context, index) => EntityCard(
+                  entityName: '하나숭이',
+                  animalName: '원숭이',
+                  animalCategory: '포유류',
+                  onTap: () {
+                    context.push('/entity-info/detail', extra: 1);
+                  },
+                ),
+                separatorBuilder: (context, index) => const SizedBox(height: 8),
+              ),
             ),
           ],
         ),
