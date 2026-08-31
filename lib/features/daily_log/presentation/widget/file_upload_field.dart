e847daba6a8783690ec7ab5f6caf47aedef2bcaf
leejia0324 <hx.un0707@gmail.com>
2026-08-31 21:16:01 +0900

    feat: 업무일지 파일 업로드 컴포넌트 추가

---
+++ lib/features/daily_log/presentation/widget/file_upload_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:toy_village_app/features/task/data/model/report_attachment.dart';
+import 'package:toy_village_app/features/task/presentation/widget/attachment_editor.dart';
+import 'package:toy_village_app/features/task/presentation/widget/attachment_picker.dart';
+
+class FileUploadField extends ConsumerStatefulWidget {
+  final ValueChanged<List<ReportAttachment>>? onChanged;
+
+  const FileUploadField({super.key, this.onChanged});
+
+  @override
+  ConsumerState<FileUploadField> createState() => _FileUploadFieldState();
+}
+
+class _FileUploadFieldState extends ConsumerState<FileUploadField> {
+  final List<ReportAttachment> _files = [];
+
+  Future<void> _add() async {
+    final attachment = await pickAndUploadAttachment(context, ref);
+    if (attachment == null) return;
+    setState(() => _files.add(attachment));
+    widget.onChanged?.call(List.unmodifiable(_files));
+  }
+
+  void _delete(int index) {
+    setState(() => _files.removeAt(index));
+    widget.onChanged?.call(List.unmodifiable(_files));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AttachmentEditor(files: _files, onAdd: _add, onDelete: _delete);
+  }
+}
