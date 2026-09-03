1773f53d2a0500fe88798a4394ff9edad9890636
leejia0324 <hx.un0707@gmail.com>
2026-09-03 11:21:29 +0900

    refactor: 먹이 급여 종 목록 뷰모델 연결

--- lib/features/feed/feed_writing/presentation/view/feed_writing_list_view.dart
+++ lib/features/feed/feed_writing/presentation/view/feed_writing_list_view.dart
@@ -1,42 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:toy_village_app/core/widgets/app_bar/app_bar.dart';
 import 'package:toy_village_app/core/widgets/chip/category_filter.dart';
 import 'package:toy_village_app/core/widgets/text/title.dart';
+import 'package:toy_village_app/features/feed/feed_info/presentation/view_model/feed_species_view_model.dart';
 import 'package:toy_village_app/features/feed/feed_writing/presentation/widget/feed_animal_card.dart';
 
-class _AnimalSpecies {
-  final String speciesName;
-  final String category;
-
-  const _AnimalSpecies({required this.speciesName, required this.category});
-}
-
-const _species = <_AnimalSpecies>[
-  _AnimalSpecies(speciesName: '카피바라', category: '포유류'),
-  _AnimalSpecies(speciesName: '원숭이', category: '포유류'),
-  _AnimalSpecies(speciesName: '사막여우', category: '포유류'),
-  _AnimalSpecies(speciesName: '흰동가리', category: '어류'),
-  _AnimalSpecies(speciesName: '금붕어', category: '어류'),
-  _AnimalSpecies(speciesName: '거북', category: '파충류'),
-  _AnimalSpecies(speciesName: '이구아나', category: '파충류'),
-  _AnimalSpecies(speciesName: '비둘기', category: '조류'),
-  _AnimalSpecies(speciesName: '앵무', category: '조류'),
-];
-
-class FeedWritingListView extends StatefulWidget {
+class FeedWritingListView extends ConsumerStatefulWidget {
   const FeedWritingListView({super.key});
 
   @override
-  State<FeedWritingListView> createState() => _FeedWritingListViewState();
+  ConsumerState<FeedWritingListView> createState() =>
+      _FeedWritingListViewState();
 }
 
-class _FeedWritingListViewState extends State<FeedWritingListView> {
+class _FeedWritingListViewState extends ConsumerState<FeedWritingListView> {
   String _category = animalCategories.first;
 
   @override
   Widget build(BuildContext context) {
-    final species = _species.where((e) => e.category == _category).toList();
+    final species = ref
+        .watch(feedSpeciesViewModelProvider)
+        .where((e) => e.category == _category)
+        .toList();
 
     return Scaffold(
       appBar: const ToyVillageAppBar(hasIcon: true),
