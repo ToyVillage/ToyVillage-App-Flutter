15ecdb6e74cf7666c48d730daef7da1ffd11bf51
leejia0324 <hx.un0707@gmail.com>
2026-09-15 00:34:01 +0900

    feat: 업무일지 목록 레포지토리 추가

---
+++ lib/features/daily_log/data/repository/daily_log_summary_repository.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:toy_village_app/core/network/api_endpoints.dart';
+import 'package:toy_village_app/core/network/dio_provider.dart';
+import 'package:toy_village_app/features/daily_log/data/model/daily_log_summary.dart';
+
+final dailyLogSummaryRepositoryProvider = Provider(
+  (ref) => DailyLogSummaryRepository(ref.read(dioProvider)),
+);
+
+class DailyLogSummaryRepository {
+  final Dio _dio;
+
+  DailyLogSummaryRepository(this._dio);
+
+  Future<List<DailyLogSummary>> loadMyWorkLogs({
+    int page = 0,
+    int size = 10,
+    String sort = 'writeAt,desc',
+  }) async {
+    final response = await _dio.get(
+      ApiEndpoints.workLogEmployee,
+      queryParameters: {'page': page, 'size': size, 'sort': sort},
+    );
+    final data = response.data;
+    final list =
+        (data is Map<String, dynamic> ? data['content'] : data) as List;
+    return list
+        .map((e) => DailyLogSummary.fromJson(e as Map<String, dynamic>))
+        .toList();
+  }
+}
