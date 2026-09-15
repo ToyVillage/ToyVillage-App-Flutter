b7a1ebfe0c1283f7e53d5789b6885e5e91629526
leejia0324 <hx.un0707@gmail.com>
2026-09-15 23:54:13 +0900

    feat: 업무일지 조회 뷰모델 API 연동

--- lib/features/daily_log/presentation/view_model/my_daily_log_view_model.dart
+++ lib/features/daily_log/presentation/view_model/my_daily_log_view_model.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:toy_village_app/features/daily_log/data/model/daily_log_summary.dart';
+import 'package:toy_village_app/features/daily_log/data/repository/daily_log_summary_repository.dart';
 
 final myDailyLogViewModelProvider =
     AsyncNotifierProvider<MyDailyLogViewModel, List<DailyLogSummary>>(
@@ -10,27 +11,6 @@ final myDailyLogViewModelProvider =
 class MyDailyLogViewModel extends AsyncNotifier<List<DailyLogSummary>> {
   @override
   FutureOr<List<DailyLogSummary>> build() {
-    return [
-      DailyLogSummary(
-        workLogId: 1,
-        writeAt: DateTime(2026, 8, 8, 12),
-        templateTitle: '먹이급여일지',
-      ),
-      DailyLogSummary(
-        workLogId: 2,
-        writeAt: DateTime(2026, 8, 7, 18, 30),
-        templateTitle: '건강관리일지',
-      ),
-      DailyLogSummary(
-        workLogId: 3,
-        writeAt: DateTime(2026, 8, 6, 9, 15),
-        templateTitle: '사육장점검일지',
-      ),
-      DailyLogSummary(
-        workLogId: 4,
-        writeAt: DateTime(2026, 8, 5, 20),
-        templateTitle: '마감일지',
-      ),
-    ];
+    return ref.read(dailyLogSummaryRepositoryProvider).loadMyWorkLogs();
   }
 }
