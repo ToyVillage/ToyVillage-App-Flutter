fc7179ce93fe9b8fa8a8f3fadef1112b5304702d
leejia0324 <hx.un0707@gmail.com>
2026-09-18 19:22:45 +0900

    test: 자동로그인 도입에 맞춰 초기 진입(스플래시→로그인) 테스트 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toy_village_app/main.dart';
 
 void main() {
-  testWidgets('앱이 로그인 화면으로 렌더된다', (WidgetTester tester) async {
+  testWidgets('토큰이 없으면 스플래시 후 로그인 화면으로 이동한다', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const ProviderScope(child: ToyVillageApp()));
     await tester.pump();
+    await tester.pump(const Duration(seconds: 2));
+    await tester.pumpAndSettle();
 
     expect(find.text('로그인'), findsWidgets);
   });
